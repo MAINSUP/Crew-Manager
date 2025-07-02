fe8117b1720e081ac6880ea71185bdd2a110a1e3
Maksym Stetsenko <115939970+MAINSUP@users.noreply.github.com>
2025-07-02 12:04:49 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -4,5 +4,6 @@ from oauth2client.service_account import ServiceAccountCredentials
 # AUTH
 def authenticate():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("D:\PycharmProjects\Crew_manager\credentials.json", scope)
+    creds_dict = st.secrets["google"]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     return creds
