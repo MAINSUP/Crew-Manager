8d6d68ed1b25961f093cf2374e4442ef0db59bda
Maksym Stetsenko <115939970+MAINSUP@users.noreply.github.com>
2025-07-02 16:51:44 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,11 +1,11 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
-
+print(st.secrets["google"]["type"])
 
 # AUTH
 def authenticate():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds_dict = st.secrets[google]
+    creds_dict = st.secrets["google"]
     print(creds_dict)
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     return creds
